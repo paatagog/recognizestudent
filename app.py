6d61290d47df38ae6075396aeea754ce445f4dc1
paatagog <paatagog@gmail.com>
2020-04-09 00:04:49 +0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(name=None):
+    return render_template('index.html',name=name)
+
+@app.route('/exec')
+def parse(name=None):
+    print("done")
+    return render_template('index.html',name=name)
+
+@app.route('/exec2')
+def parse1(name=None):
+	print("done")
+	return render_template('index.html',name=name)
+
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
